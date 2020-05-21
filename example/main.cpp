0c0a28bc95e0aabe0ea5a1c79cc8581563afbe12
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-05-22 00:08:15 +0100

    Add benchmark for example application

--- example/main.cpp
+++ example/main.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
+#include <chrono>
 
 #include <state.h>
 #include <transform.h>
 
 void printUsage(char* commandName, std::ostream& os = std::cout) {
-    os << "Usage: " << commandName << " WIDTH HEIGHT\n"
-       << "Grid should then be provided through stdin.\n";
+    os << "Usage: " << commandName << " WIDTH HEIGHT [N]\n"
+       << "N is the number of iterations to perform, default is 1\n"
+       << "Grid data should then be provided through stdin.\n";
 }
+
+life::Grid advance(life::Grid src, size_t n) {
+    life::Grid dst{src.rows(), src.columns()};
+    while (n >= 2) {
+        life::Transform(src, dst);
+        life::Transform(dst, src);
+        n -= 2;
+    }
+    if (n == 1) {
+        life::Transform(src, dst);
+        return dst;
+    } else {
+        return src;
+    }
+}
+
+life::Grid advance(const life::Grid& src) {
+    life::Grid dst{src.rows(), src.columns()};
+    life::Transform(src, dst);
+    return dst;
+}
+
+const auto timeIt = [](auto func){
+    auto start = std::chrono::high_resolution_clock::now();
+    auto ret = func();
+    auto finish = std::chrono::high_resolution_clock::now();
+
+    auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(finish-start);
+
+    return std::make_pair(std::move(ret), microseconds);
+};
 
 int main(int argc, char** argv) {
     if (argc <= 2) {
@@ -21,8 +54,13 @@ int main(int argc, char** argv) {
 
     std::cin >> grid;
 
-    life::Grid nextState{height, width};
-    life::Transform(grid, nextState);
+    if (argc > 3) {
 
-    std::cout << nextState;
+        size_t N = std::stoull(argv[3]);
+        auto [final, time] = timeIt([&]{return advance(grid, N);});
+        std::cout << final;
+        std::cerr << "Took " << time.count() << "µs\n";
+    } else {
+        std::cout << advance(grid);
+    }
 }
